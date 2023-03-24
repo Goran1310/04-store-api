c9c9629a55bd77d966944e619d9f06da5521f8ba
Goran Lovincic <lovincic@gmail.com>
2023-03-24 09:01:10 +0100

    $regex: name, $options: 'i'

--- controllers/products.js
+++ controllers/products.js
@@ -1,20 +1,26 @@
 const Product = require('../models/product')
 
 const  getAllProductsStatic  =   async   ( req ,  res )   =>   {
+    const search  =   'ab'
     const products  =   await  Product.find ( { 
-        name : 'vase table'
+        name : { $regex: search, $options: 'i' }
      } )
     res.status ( 200 ) .json ( {products, nbHits: products.length} )
     // throw new  Error ( 'TESTING ASYNC error from get all products static' )
     // res.status ( 200 ) .json ( {msg: 'get all products' } )
 }
 const getAllProducts  =   async   ( req ,  res )   =>   {
-    const { featured ,  company ,  name ,  sort ,  fields ,  numericFilters }  =  req.query
-    // const products =   await  Product.find (  req.query);
+    const { featured ,  company, name, search}  =  req.query
     const queryObject  =   { }
 
     if ( featured ) {
         queryObject.featured  =  featured  ===   'true'   ?  true  :  false
+    }
+    if ( company ) {
+        queryObject.company  =  company
+    }
+    if ( name ) {
+        queryObject.name  =  { $regex: name, $options: 'i' }
     }
     console.log ( queryObject )
     const products =   await  Product.find (queryObject);
