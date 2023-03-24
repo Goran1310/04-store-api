208c46bbd9a7bbbeb3537352ad15032251d7fb45
Goran Lovincic <lovincic@gmail.com>
2023-03-24 05:49:29 +0100

    getting product from mongoDB

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 
 
-
+https://youtu.be/qwfE7fSVaZM?t=13953
 https://hn.algolia.com/
 https://cloud.mongodb.com/v2/634d341e2bd7d4241377ce34#/clusters
+https://www.npmjs.com/package/express-async-errors
+https://mongoosejs.com/docs/queries.html
--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,10 @@
-
+const Product = require('../models/product')
 
 const  getAllProductsStatic  =   async   ( req ,  res )   =>   {
-    res.status ( 200 ) .json ( {msg: 'get all products' } )
+    const products  =   await  Product.find ( { } )
+    res.status ( 200 ) .json ( {products} )
+    // throw new  Error ( 'TESTING ASYNC error from get all products static' )
+    // res.status ( 200 ) .json ( {msg: 'get all products' } )
 }
 const getAllProducts  =   async   ( req ,  res )   =>   {
     res.status ( 200 ) .json ( {msg: 'products route' } )
