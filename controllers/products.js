a92523a69c13843e9192353b1afeed5d26d2d3d3
Goran Lovincic <lovincic@gmail.com>
2023-03-24 10:53:07 +0100

    numericFilters + const operatorMap

--- controllers/products.js
+++ controllers/products.js
@@ -3,19 +3,20 @@ const Product = require('../models/product')
 const  getAllProductsStatic  =   async   ( req ,  res )   =>   {
     // const search  =   'ab'
     const products  =   await  Product.find ( { 
+        price : { $gt : 30 } 
         // name : { $regex: search, $options: 'i' }
      } ).
-    sort ( 'name' ).
-    select ( 'name price' ).
-    limit ( 10 ).
-    skip ( 2 )
+    sort ( 'price' ).
+    select ( 'name price' )
+    // limit ( 10 ).
+    // skip ( 2 )
 
     res.status ( 200 ) .json ( {products, nbHits: products.length} )
     // throw new  Error ( 'TESTING ASYNC error from get all products static' )
     // res.status ( 200 ) .json ( {msg: 'get all products' } )
 }
 const getAllProducts  =   async   ( req ,  res )   =>   {
-    const { featured ,  company, name, sort , fields }  =  req.query
+    const { featured ,  company, name, sort , fields, numericFilters }  =  req.query
     const queryObject  =   { }
     if ( featured ) {
         queryObject.featured  =  featured  ===   'true'   ?  true  :  false
@@ -26,7 +27,17 @@ const getAllProducts  =   async   ( req ,  res )   =>   {
     if ( name ) {
         queryObject.name  =  { $regex: name, $options: 'i' }
     }
-    // console.log ( queryObject )
+    if ( numericFilters ) {
+        const operatorMap  =   {
+            '>' : '$gt' ,
+            '>=' : '$gte' ,
+            '=' : '$eq' ,
+            '<' : '$lt' ,
+            '<=' : '$lte' ,
+        }
+        console.log ( numericFilters )
+    }
+    console.log ( queryObject )
     let result = Product.find (queryObject)
     // sort
     if (sort) {
@@ -35,10 +46,16 @@ const getAllProducts  =   async   ( req ,  res )   =>   {
       } else {
         result = result.sort('createdAt');
       }
-    if(field) {
+    if(fields) {
         const fieldsList = fields.split(',').join(' ');
-        result = result.select(fieldList);
+        result = result.select(fieldsList);
     }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit)
+
+
     const products = await result
     res.status ( 200 ) .json ( {products, nbHits: products.length} )
     // console.log ( req .query )
