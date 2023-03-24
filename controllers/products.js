d7418a12b49fe026d1a02aeb5032a5e91343f278
Goran Lovincic <lovincic@gmail.com>
2023-03-24 06:20:37 +0100

    filtering objects

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,26 @@
 const Product = require('../models/product')
 
 const  getAllProductsStatic  =   async   ( req ,  res )   =>   {
-    const products  =   await  Product.find ( { } )
-    res.status ( 200 ) .json ( {products} )
+    const products  =   await  Product.find ( { 
+        name : 'vase table'
+     } )
+    res.status ( 200 ) .json ( {products, nbHits: products.length} )
     // throw new  Error ( 'TESTING ASYNC error from get all products static' )
     // res.status ( 200 ) .json ( {msg: 'get all products' } )
 }
 const getAllProducts  =   async   ( req ,  res )   =>   {
-    res.status ( 200 ) .json ( {msg: 'products route' } )
+    const { featured ,  company ,  name ,  sort ,  fields ,  numericFilters }  =  req.query
+    // const products =   await  Product.find (  req.query);
+    const queryObject  =   { }
+
+    if ( featured ) {
+        queryObject.featured  =  featured  ===   'true'   ?  true  :  false
+    }
+    console.log ( queryObject )
+    const products =   await  Product.find (queryObject);
+    res.status ( 200 ) .json ( {products, nbHits: products.length} )
+    // console.log ( req .query )
+    // res.status ( 200 ) .json ( {msg: 'products route' } )
 }
 
 module . exports  =  {
